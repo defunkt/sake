9a61a4de130b266ecc559771841d7ea5c8705273
Chris Wanstrath <chris@ozmm.org>
2007-06-25 09:04:01 +0000

    dont need to be this clever

--- lib/server.rb
+++ lib/server.rb
@@ -1,24 +1,20 @@
 require 'sake' unless defined? Sake
+require 'mongrel'
 
 class Sake
   module Server
     extend self
 
     def start(args)
-      require 'mongrel'
-
       if index = args.index('-p')
         port = args[index+1].to_i
       else
         port = 4567
       end
 
-      handler   = handler_class
-      daemonize = args.include? '-d'
-
       config = Mongrel::Configurator.new :host => "127.0.0.1" do
-        listener(:port => port) { uri "/", :handler => handler.new }
-        daemonize(:cwd => '.', :log_file => 'sake.log') if daemonize
+        listener(:port => port) { uri "/", :handler => Handler.new }
+        daemonize(:cwd => '.', :log_file => 'sake.log') if args.include? '-d'
         run
       end
 
@@ -26,13 +22,7 @@ class Sake
       config.join
     end
 
-    # We don't want to require mongrel if you're not gonna use
-    # it, y'know?
-    def handler_class
-      Class.new(Mongrel::HttpHandler) { include Handler }
-    end
-
-    module Handler 
+    class Handler < Mongrel::HttpHandler
       def process(request, response)
         uri    = request.params['PATH_INFO'].sub(/^\//, '')
         status = uri.empty? ? 200 : 404
