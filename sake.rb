750b15855e3bac03b6f4e601c9bc52d4346afbdb
Chris Wanstrath <chris@ozmm.org>
2007-06-25 09:03:44 +0000

    which file?

--- sake.rb
+++ sake.rb
@@ -5,7 +5,7 @@ require 'rake'
 require 'fileutils'
 
 class Sake
-  def initialize(args)
+  def initialize(args = [])
     ensure_task_directory_exists
     @args   = args
     @target = detect_target(args)
@@ -21,18 +21,22 @@ class Sake
   def install
     if target_tasks.size != target_tasks.uniq.size
       puts "Can't install with duplicate tasks.  The following tasks are duped in #{@target}: "
-      die (target_tasks & target_tasks).map { |task| "  #{task}" }
+      die with_indent(target_tasks & target_tasks)
     end
 
     target_tasks.each do |task|
       if sake_tasks.include?(task) 
-        die "Doh! You already have an installed task named `#{task}'.  Please rename the one in #{@target}."
+        die "Doh! You already have an installed task named `#{task}'.  Please rename the task in #{@target} or #{task.sake_file}."
       end
     end
 
     FileUtils.cp @target, fresh_task_path(@target)
     puts "Installed these tasks: "
-    puts "  #{target_tasks}"
+    puts with_indent(target_tasks)
+  end
+
+  def with_indent(array)
+    array.map { |task| "  #{task}" }
   end
 
   ##
@@ -85,6 +89,12 @@ class Sake
     Dir["#{task_directory}/*.rake"]
   end
 
+  def sake_file(string)
+    task_files.detect do |file|
+      RakeFaker.new(file).tasks.include? string
+    end
+  end
+
   def task_directory
     File.join(File.expand_path('~'), '.sake')
   end
@@ -106,8 +116,8 @@ class Sake
     end
   end
 
-  def die(message)
-    puts(message) || exit
+  def die(*message)
+    puts(*message) || exit
   end
 
   class RakeFaker
@@ -128,6 +138,12 @@ class Sake
     def task(name)
       @tasks << [ @namespace, name ].flatten * ':'
     end
+  end
+end
+
+class String
+  def sake_file
+    Sake.new.sake_file(self)
   end
 end
 
